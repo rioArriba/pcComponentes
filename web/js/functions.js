3c39c3e5bfd344e463e3b8bdc7aec36eed380896
wendy aguilar <wendylizbethaguilarcoyago.com>
2022-12-16 08:51:38 +0100

    feat(1.8): footer

---
+++ web/js/functions.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+    document.getElementById("c-barra").onclick = function () {
+        let nav = document.getElementsByClassName("c-nav")[0];
+        if(nav.style.display=="inline-block"){
+            nav.style='display:none;'
+        }else{
+            nav.style = 'display:inline-block;';
+        } 
+    }
+}
+
+function pulsaCarrito(){
+    document.getElementById("miCarrito").showModal();
+}
